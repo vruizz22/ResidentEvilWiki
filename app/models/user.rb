3ba9c2499e99ca6f7d67c3c324af61a1fc89b7be
VICTOR RUIZ <veruiz@uc.cl>
2025-04-18 17:36:14 -0400

    Commit final de rama feature registro de usuarios, se configuro device para el registro de usuarios, comprobado su funcionamiento, ademas se agregaron los atributos especificados en E/R

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Asociación con Active Storage
+  has_one_attached :imagen_perfil
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }, 
+  path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
