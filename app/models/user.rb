89e403e31b8d1de448e853c2605e16d8cdc9d844
VICTOR RUIZ <veruiz@uc.cl>
2025-04-18 23:57:37 -0400

    validaciones en user.rb para descripcion y

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,22 @@ class User < ApplicationRecord
 
   # Asociación con Active Storage
   has_one_attached :imagen_perfil
+
+  # Validaciones
+  validates :descripcion, length: { maximum: 500 }
+  validate :validate_imagen_perfil
+
+  # Método Privado para validar el formato y tamaño de la imagen
+  private
+  def validate_imagen_perfil
+    return unless imagen_perfil.attached?
+    
+    if imagen_perfil.blob.byte_size > 5.megabytes
+      errors.add(:imagen_perfil, "es demasiado grande (máximo 5MB)")
+    end
+    
+    unless imagen_perfil.blob.content_type.in?(%w(image/jpeg image/png))
+      errors.add(:imagen_perfil, "debe ser JPG o PNG")
+    end
+  end
 end
