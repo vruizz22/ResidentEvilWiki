1aed4598822dfdc121639fe94d2497fc63566799
VICTOR RUIZ <veruiz@uc.cl>
2025-05-17 00:24:38 -0400

    hotfix render

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,4 @@ Rails.application.config.assets.version = "1.0"
 
 # config/initializers/assets.rb
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
+Rails.application.config.assets.precompile += %w( application.scss )
