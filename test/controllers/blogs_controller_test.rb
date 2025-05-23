3ff9a89639d32e7b56074e388fd142ecd808dfcb
VICTOR RUIZ <veruiz@uc.cl>
2025-05-23 15:51:59 -0400

    test(blogs_controller_test y home_controller_test/index muestra lista de blogs para usuario no autenticado): se cambia test que evalua si un usuario no registrado puede ver los blogs desde home_controller_test a blogs_controller_test

--- test/controllers/blogs_controller_test.rb
+++ test/controllers/blogs_controller_test.rb
@@ -15,6 +15,20 @@ class BlogsControllerTest < ActionDispatch::IntegrationTest
     )
   end
 
+  test "index muestra lista de blogs para usuario no autenticado" do
+    # Creamos dos blogs con distinto created_at
+    b1 = Blog.create!(titulo: 'título uno', descripcion: 'x', tipo_publicacion: 'noticia', estado: 'aprobado', 
+                      created_at: 2.days.ago)
+    b2 = Blog.create!(titulo: 'título dos', descripcion: 'y', tipo_publicacion: 'reseña', estado: 'aprobado', 
+                      created_at: 1.day.ago)
+
+    get blogs_url
+    assert_response :success
+    # El último creado debe ir primero
+    assert_select 'div.column.is-one-third:first-of-type .card-header-title', text: b2.titulo
+    assert_select 'div.column.is-one-third:last-of-type .card-header-title', text: b1.titulo
+  end
+
   test "index asigna sólo blogs aprobados" do
     approved = Blog.create!(
       titulo: 'yyyyyyyyyy',
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -7,17 +7,4 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     @user = User.create!(email: 'user@example.com', password: '123456')
   end
 
-  test "index muestra lista de blogs para usuario no autenticado" do
-    # Creamos dos blogs con distinto created_at
-    b1 = Blog.create!(titulo: 'título uno', descripcion: 'x', tipo_publicacion: 'noticia', estado: 'aprobado', 
-                      created_at: 2.days.ago)
-    b2 = Blog.create!(titulo: 'título dos', descripcion: 'y', tipo_publicacion: 'reseña', estado: 'aprobado', 
-                      created_at: 1.day.ago)
-
-    get root_url
-    assert_response :success
-    # El último creado debe ir primero
-    assert_select 'div.column.is-one-third:first-of-type .card-header-title', text: b2.titulo
-    assert_select 'div.column.is-one-third:last-of-type .card-header-title', text: b1.titulo
-  end
 end
