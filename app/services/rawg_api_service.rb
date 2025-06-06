9a088af2e91cb152519f57931e9bf82b4b9c64d1
VICTOR RUIZ <veruiz@uc.cl>
2025-06-06 17:43:05 -0400

    feat(blogs): Integrar información de juegos desde la API de RAWG en las vistas de blogs

---
+++ app/services/rawg_api_service.rb
@@ -0,0 +1,39 @@
+# Servicio para consultar la API de RAWG y obtener información de un videojuego por nombre
+require 'net/http'
+require 'json'
+
+class RawgApiService
+  BASE_URL = 'https://api.rawg.io/api/games'
+
+  def initialize(api_key)
+    @api_key = api_key
+  end
+
+  # Busca un juego por nombre y retorna los datos principales
+  def search_game(game_name)
+    encoded_name = URI.encode_www_form_component(game_name)
+    url = URI("#{BASE_URL}?search=#{encoded_name}&key=#{@api_key}")
+    response = Net::HTTP.get(url)
+    data = JSON.parse(response)
+    return nil if data['results'].blank?
+    game = data['results'].first
+    {
+      name: game['name'],
+      released: game['released'],
+      rating: game['rating'],
+      ratings_count: game['ratings_count'],
+      background_image: game['background_image'],
+      description: fetch_game_description(game['id'])
+    }
+  end
+
+  private
+
+  # Obtiene la descripción completa del juego por ID
+  def fetch_game_description(game_id)
+    url = URI("#{BASE_URL}/#{game_id}?key=#{@api_key}")
+    response = Net::HTTP.get(url)
+    data = JSON.parse(response)
+    data['description_raw'] || ''
+  end
+end
